fcd2ccee526e0b68e9389b83e3f614e700f4f3e9
VGUCrew1999 <kenkfugate99@gmail.com>
2023-03-21 18:24:19 -0400

    Initial Commit

---
+++ Currency.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyCalculator
+{
+    internal class Currency
+    {
+        public string Name { get; set; }
+        public double ConversionRate { get; set; }
+
+
+    }
+}
